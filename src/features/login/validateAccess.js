e73104b4cc04026c4bea7efac85b68c39f005a08
lhervi@gmail.com <lhervi@gmail.com>
2020-12-19 01:04:58 -0400

    Al fingit add . pero todavía falta :)

--- src/features/login/validateAccess.js
+++ src/features/login/validateAccess.js
@@ -5,58 +5,71 @@ import axios from 'axios';
 function validateAccess(e, email, password){
 
   e.preventDefault();
-  console.log(`Bienvenido a valida Access..  vamos a comprobar si el email: ${email} y el password ${password} están registrados`);
-
+ 
   //const cookies = new cookies();
-  const url = "http://localhost:3001/users";
-  const userInfo = {username: email, password: password};
-  console.log(`Información del user: ${JSON.stringify(userInfo)}`);
+  const urlApi = "http://localhost:3001/users";
+  const userInfo = {email: email, password: password};  
 
-  const prueba = async ()=> {
-    await axios.get(url)
-      .then(response => console.log(response.data))};
+  axios.get(urlApi, {params: userInfo})
+    .then(response=>{
+      console.log(response);
+      if (response.data.length>0){
+        console.log("Encontrado!!!");
+        const {id, middle, lastname ,name, username, email} = response.data[0];
+        console.log(id, middle, lastname ,name, username, email);
+                    
+      }else{
+        console.log("No se encontró el usuario");
+      }
+    },(error)=>{
+      console.log(error)
+    });
   
-  const startSesion = async () => {
-    await axios.get(url, {params: userInfo})  
-      .then (response => {
-        console.log(response.data);
-      })
-    .catch(error => {        
-        console.log(error);
-    })
-  } 
+  
+ //fetch (url, {method:'GET', params: userInfo})
+/*  fetch (request)
+      .then(resp => resp.json())
+        .then(r => console.log(r))
+      //    .catch(e=>console.log(e)); */ 
 
-  console.log(JSON.stringify(startSesion));
-  return startSesion
+  
+  //return connect()
 }
-export default validateAccess;
+ 
+export default validateAccess
 
-/*{"users":[
-    {
-        "id": 1,
-        "Middle": "García",
-        "Lastname": "Ramirez",
-        "name": "Guillermo",
-        "username": "cgarcia",
-        "email": "cgarcia@test.com",
-        "password": "827ccb0eea8a706c4c34a16891f84e7b"
-    },
-    {
-        "id": 2,
-        "Middle": "Ferandez",
-        "Lastname": "Flores",
-        "name": "Roberto",
-        "username": "rflores",
-        "email": "rflores@test.com",
-        "password": "01cfcd4f6b8770febfb40cb906715822"
-    },
-    {
-        "id": 3,
-        "Middle": "Serrano",
-        "Lastname": "Carrera",
-        "name": "Mirian",
-        "username": "mcarrera",
-        "email": "mcarrera@test.com",
-        "password": "e10adc3949ba59abbe56e057f20f883e"
-    }
-]}*/  
+/* Archivo JSON para la API Fake
+  {
+    "users":[
+        {
+            "id": 1,
+            "middle": "García",
+            "lastname": "Ramirez",
+            "name": "Guillermo",
+            "username": "cgarcia",
+            "email": "cgarcia@test.com",
+            "password": "827ccb0eea8a706c4c34a16891f84e7b"
+        },
+        {
+            "id": 2,
+            "middle": "Ferandez",
+            "lastname": "Flores",
+            "name": "Roberto",
+            "username": "rflores",
+            "email": "rflores@test.com",
+            "password": "01cfcd4f6b8770febfb40cb906715822"
+        },
+        {
+            "id": 3,
+            "middle": "Serrano",
+            "lastname": "Carrera",
+            "name": "Mirian",
+            "username": "mcarrera",
+            "email": "mcarrera@test.com",
+            "password": "e10adc3949ba59abbe56e057f20f883e"
+        }
+    ]          
+
+}
+
+*/  
